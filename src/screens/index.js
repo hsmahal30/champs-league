ae40afb6c939c3be9c8f2ec8253d3b5f4c0b5d8c
Harpreet Mahal <hsmahal00@gmail.com>
2021-04-20 11:10:04 -0400

    new files

---
+++ src/screens/index.js
@@ -0,0 +1,4 @@
+export {default as LoginScreen} from './LoginScreen/LoadingScreen'
+export {default as HomeScreen} from './HomeScreen/HomeScreen'
+export {default as RegisterScreen} from './RegisterScreen/RegisterScreen'
+
