d381a30cce6331c3fce14cf0f368faa8bd2d4c0f
Harpreet Mahal <hsmahal00@gmail.com>
2021-04-20 11:07:15 -0400

    New File Structure

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, {useState, useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {LoginScreen, HomeScreen, RegistrationScreen} from './src/screens'
+import {decode, encode} from 'base-64'
 import { StyleSheet, Text, View , Image} from 'react-native';
 import LoginPage from './LoginPage'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+  
   return (
     <View style={styles.container}>
       <LoginPage />
