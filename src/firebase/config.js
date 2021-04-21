8003be0514262bdef0f0de3ef898a9508daab752
Harpreet Mahal <hsmahal00@gmail.com>
2021-04-20 21:48:14 -0400

    removed file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,7 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2HwMG3twiiZJuGcAz6n7ASicdt5-Gmq8",
-    authDomain: "champions-league-578dc.firebaseapp.com",
-    projectId: "champions-league-578dc",
-    storageBucket: "champions-league-578dc.appspot.com",
-    messagingSenderId: "591882160432",
-    appId: "1:591882160432:web:59d5fd8829959a4261a0f5"
+    
   };
 
 if (!firebase.apps.length) {
